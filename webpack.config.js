b0065eb3b985ff66e6acce03fdca67f5898c87e5
rdoo <rafdomaradzki@gmail.com>
2020-05-03 22:32:21 +0200

    Migrate to ts-loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,40 +1,46 @@
 const path = require('path');
 const fs = require('fs');
 
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
 const RunNodeWebpackPlugin = require('run-node-webpack-plugin');
 
 const nodeModules = {};
-fs.readdirSync('node_modules').filter(moduleName => moduleName !== '.bin').forEach(moduleName => nodeModules[moduleName] = 'commonjs ' + moduleName);
+fs.readdirSync('node_modules')
+  .filter((moduleName) => moduleName !== '.bin')
+  .forEach((moduleName) => (nodeModules[moduleName] = 'commonjs ' + moduleName));
 
 module.exports = (env, argv) => {
-    const IS_PRODUCTION_BUILD = argv.mode === 'production';
+  const IS_PRODUCTION_BUILD = argv.mode === 'production';
 
-    const webpackPlugins = [];
+  const webpackPlugins = [new ForkTsCheckerWebpackPlugin()];
 
-    if (!IS_PRODUCTION_BUILD) {
-        webpackPlugins.push(new RunNodeWebpackPlugin());
-    }
+  if (!IS_PRODUCTION_BUILD) {
+    webpackPlugins.push(new RunNodeWebpackPlugin());
+  }
 
-    return {
-        target: 'node',
-        externals: nodeModules,
-        node: {
-            __filename: false,
-            __dirname: false
+  return {
+    target: 'node',
+    externals: nodeModules,
+    node: {
+      __filename: false,
+      __dirname: false,
+    },
+    entry: path.join(__dirname, 'src', 'index.ts'),
+    output: {
+      path: path.join(__dirname, 'build'),
+      filename: 'bundle.js',
+    },
+    resolve: {
+      extensions: ['.ts', '.js'],
+    },
+    module: {
+      rules: [
+        {
+          test: /\.ts$/,
+          loaders: [{ loader: 'ts-loader', options: { transpileOnly: true } }],
         },
-        entry: path.join(__dirname, 'src', 'index.ts'),
-        output: {
-            path: path.join(__dirname, 'build'),
-            filename: 'bundle.js'
-        },
-        resolve: {
-            extensions: ['.ts']
-        },
-        module: {
-            rules: [
-                { test: /\.ts$/, loaders: ['awesome-typescript-loader'] }
-            ]
-        },
-        plugins: webpackPlugins
-    };
+      ],
+    },
+    plugins: webpackPlugins,
+  };
 };
